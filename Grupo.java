9c21313a2937bd7144144ace8289fb24faa046bc
De La Torre Lara Arturo <arturo.delato@hotmail.com>
2017-08-29 11:04:55 -0500

    Version ejercicio arreglos se uso this. y metodos auxiliares

--- Grupo.java
+++ Grupo.java
@@ -9,27 +9,29 @@
          this.nombreMateria = nombreMateria;
      }
      
-     public void inscribir(Estudiante unEstudiante)
-     {
-         for(int i = 0; i < estudiantes.length; i++)
+     /**
+        Inscribe un estudiante nuevo en el grupo.
+        @param unEstudiante Es el objeto estudiante a inscribir en el grupo
+        @return Regresa verdadero(true) si el estudiante fue inscrito o falso(false) en caso de que no sea incrito
+     **/
+     public boolean inscribir(Estudiante unEstudiante)
+     {   
+         int existe = this.buscaEstudiante(unEstudiante.dimeClave());
+         
+         if(existe != -1)
          {
-             if(estudiantes[i] == null)
-             {
-                 estudiantes[i] = unEstudiante;
-                 i = estudiantes.length;
-             }    
+             return false; //El estudiante ya esta inscrito
          }
-     }
- 
-     public void imprimeEstudiates()
-     {
-         for(int i = 0; i < estudiantes.length; i++)
+         
+         int posDisponible =  this.buscaEspacioDisponible();
+         
+         if(posDisponible == -1)
          {
-             if(estudiantes[i] != null)
-             {
-                 System.out.println(estudiantes[i].dimeDetalles());
-             }
+             return false; //El arreglo esta lleno
          }
+         
+         estudiantes[posDisponible] = unEstudiante;//Inscribe al estudiantes
+         return true;//El estudiante fue inscrito
      }
      
      //TAREA
@@ -46,5 +48,38 @@
            }
          }
          
+     }
+     
+     /**Busca un estudiante por medeio de su clave 
+        la cavle del estudiante a buscar es un parametro "claveEstudiante"
+        regresa la posicion del estudiante en el arreglo 0/1 si no esta
+     **/
+     public int buscaEstudiante(int claveEstudiante)
+     {
+         for(int i = 0; i < estudiantes.length; i++)
+         {
+             if(estudiantes[i].dimeClave() == claveEstudiante)
+             {
+                 return i;
+             }
+         }
+         
+         return -1;
+     }
+     
+     /**
+      *  busca un espacio disponible en el arreglo
+      *  @return regresa l aprimer posicion null dentro del arreglo o -1 si esta lleno.
+     **/
+     private int buscaEspacioDisponible()
+     {
+          for(int i = 0; i < estudiantes.length; i++)
+         {
+             if(estudiantes[i] == null)
+             {
+                 return i;
+             }    
+         }
+         return -1;
      }
  }
